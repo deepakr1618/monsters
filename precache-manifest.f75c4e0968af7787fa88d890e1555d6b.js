0561b166f14dc820c0d029560ac1172ea717eb9d
deepakr1618 <deepak.r1618@gmail.com>
2020-03-24 19:55:33 +0530

    Updates

---
+++ precache-manifest.f75c4e0968af7787fa88d890e1555d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad4d03a02ad1c7028b7038b2e4f0221",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "2c327eaf8a836986e7cc",
+    "url": "/monsters/static/css/main.bb65ed33.chunk.css"
+  },
+  {
+    "revision": "bb27650c9f5e6e6b07e2",
+    "url": "/monsters/static/js/2.70e63080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.70e63080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c327eaf8a836986e7cc",
+    "url": "/monsters/static/js/main.38d30ac2.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
